fedac5f81af41b2c4aa11ef232fdbf8383601174
DavidDobrev <david.dobrev0108@gmail.com>
2022-03-13 14:25:22 +0200

    First

---
+++ DobrevBusStation/Areas/Admin/Controllers/ProductTypeController.cs
@@ -0,0 +1,127 @@
+﻿using DobrevBusStation.Data;
+using DobrevBusStation.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DobrevBusStation.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypeController : Controller
+    {
+        private ApplicationDbContext _db;
+        public ProductTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var data = _db.products.ToList();
+            return View(_db.products.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult>Create(ProductType productType)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.products.Add(productType);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productType);
+        }
+        public ActionResult Edit(int?id)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+            var productType = _db.products.Find(id);
+            if (productType==null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(ProductType productType)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.products.Update(productType);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productType);
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var productType = _db.products.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Details(ProductType productType)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productType);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var productType = _db.products.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int? id,ProductType productType)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+            if (id!= productType.Id)
+            {
+                return NotFound();
+            }
+            var productTypes = _db.products.Find(id);
+            if (productTypes==null)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _db.products.Remove(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productType);
+        }
+    }
+}
---
+++ DobrevBusStation/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DobrevBusStation.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DobrevBusStation.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ProductType> products { get; set; }
+    }
+}
